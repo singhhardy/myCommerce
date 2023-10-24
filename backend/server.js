3a4fe80bec6fcf0684e2d24cc311be139b55d41d
singhhardy <singhhardy724@gmail.com>
2023-10-24 21:15:56 +0530

    integration

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
+const cors = require('cors');
 const PORT = process.env.PORT || 5000
 const connectDB = require('./Config/db')
 const { errorHandler } = require('./Middleware/errorMiddleware')
@@ -7,6 +8,8 @@ const { errorHandler } = require('./Middleware/errorMiddleware')
 connectDB()
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended : false }))
